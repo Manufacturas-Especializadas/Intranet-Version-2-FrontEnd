07e6bde21769a43a8565af7ae4e1a7a2341044ff
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-08 10:51:01 -0600

    feat(app): integracion de rutas dinamicas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { Navbar } from "./components/layout/Navbar/Navbar";
 import { Sidebar } from "./components/layout/Sidebar/Sidebar";
+import { MyRoutes } from "./routes/Routes";
 
 export const App = () => {
   return (
@@ -9,7 +10,9 @@ export const App = () => {
         <Sidebar />
         <div className="flex-1 flex flex-col overflow-hidden">
           <Navbar />
-          <main className="flex-1 overflow-x-hidden overflow-y-auto bg-[#F8F9FB] p-6"></main>
+          <main className="flex-1 overflow-x-hidden overflow-y-auto bg-[#F8F9FB] p-6">
+            <MyRoutes />
+          </main>
         </div>
       </div>
     </BrowserRouter>
