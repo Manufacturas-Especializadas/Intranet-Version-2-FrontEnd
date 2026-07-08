47153d376b8f2c5edcbf14111401f50d6d2fcad3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-08 11:30:04 -0600

    style(component): clases de tailwind para diseñar el dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import { FeaturedNewsCarousel } from "../../components/UI/FeaturedNewsCarousel/F
 
 export const Dashboard = () => {
   return (
-    <div>
+    <div className="grid grid-cols-1 lg:grid-cols-[1fr_400px] gap-6 mb-6">
       <FeaturedNewsCarousel />
     </div>
   );
