4b7123be15573e02e584f650026ff8f74c966bfd
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-08 11:44:29 -0600

    style(component): nuevo relayout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -3,9 +3,11 @@ import { QuickAccess } from "../../components/UI/QuickAccess/QuickAccess";
 
 export const Dashboard = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-[1fr_400px] gap-6 mb-6">
-      <FeaturedNewsCarousel />
-      <QuickAccess />
+    <div className="flex flex-col gap-6 w-full max-w-screen-2xl mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-[1fr_400px] gap-6">
+        <FeaturedNewsCarousel />
+        <QuickAccess />
+      </div>
     </div>
   );
 };
