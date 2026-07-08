104289755db90d169754b0db108872b42bba32ab
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-07-08 11:45:28 -0600

    feat(component): integracion de los componentes de noticias por departamento y posteos recientes

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,7 @@
+import { DepartmentNews } from "../../components/UI/DepartmentNews/DepartmentNews";
 import { FeaturedNewsCarousel } from "../../components/UI/FeaturedNewsCarousel/FeaturedNewsCarousel";
 import { QuickAccess } from "../../components/UI/QuickAccess/QuickAccess";
+import { RecentPosts } from "../../components/UI/RecentPosts/RecentPosts";
 
 export const Dashboard = () => {
   return (
@@ -7,6 +9,11 @@ export const Dashboard = () => {
       <div className="grid grid-cols-1 lg:grid-cols-[1fr_400px] gap-6">
         <FeaturedNewsCarousel />
         <QuickAccess />
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-[1fr_400px] gap-6">
+        <DepartmentNews />
+        <RecentPosts />
       </div>
     </div>
   );
